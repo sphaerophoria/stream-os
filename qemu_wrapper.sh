04380121f168bff2325d7fb020e273b3c1a9e66e
Mick Sayson <mick@sayson.com>
2023-10-14 13:44:28 -0700

    Add GDB support to qemu wrapper script

--- qemu_wrapper.sh
+++ qemu_wrapper.sh
@@ -3,6 +3,7 @@
 NOGRAPHIC=${NOGRAPHIC:-0}
 DUMP_NETWORK=${DUMP_NETWORK:-0}
 TAP_IF=${TAP_IF:-tap0}
+GDB=${GDB:-0}
 
 if [ "$NOGRAPHIC" == "0" ]; then
   STDIO_CMD="-serial stdio"
@@ -16,6 +17,12 @@ else
   DUMP_NET_CMD="-object filter-dump,id=n0,netdev=n0,file=network.dump"
 fi
 
+if [ "$GDB" == "0" ]; then
+  GDB_CMD=""
+else
+  GDB_CMD="-s -S"
+fi
+
 KERNEL="$1"
 rm -fr isodir
 mkdir -p isodir/boot/grub
@@ -23,6 +30,6 @@ cp "$KERNEL" isodir/boot/myos.bin
 cp grub.cfg isodir/boot/grub/grub.cfg
 grub-mkrescue -o myos.iso isodir 2> /dev/null
 
-qemu-system-i386 $STDIO_CMD $DUMP_NET_CMD -netdev tap,id=n0,ifname=$TAP_IF,script=no,downscript=no -device rtl8139,netdev=n0,bus=pci.0,addr=4,mac=12:34:56:78:9a:bc -device isa-debug-exit,iobase=0xf4,iosize=0x01 -cdrom myos.iso
+qemu-system-i386 $GDB_CMD $STDIO_CMD $DUMP_NET_CMD -netdev tap,id=n0,ifname=$TAP_IF,script=no,downscript=no -device rtl8139,netdev=n0,bus=pci.0,addr=4,mac=12:34:56:78:9a:bc -device isa-debug-exit,iobase=0xf4,iosize=0x01 -cdrom myos.iso
 
 exit $(($? >> 1))
